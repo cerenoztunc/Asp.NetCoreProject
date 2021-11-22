2bed21b11025ae5668a85ab289113fe34f3eaea8
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-22 21:28:08 +0300

    Creating BaseService.cs, CategoryService.cs and ProductService.cs int the Project.Service layer

---
+++ Project.Service/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using Project.Core.Models;
+using Project.Core.Repositories;
+using Project.Core.Services;
+using Project.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Services
+{
+    public class CategoryService : BaseService<Category>, ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Category> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            return await _unitOfWork.Categories.GetWithProductsByIdAsync(categoryId);
+        }
+    }
+}
---
+++ Project.Service/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using Project.Core.Models;
+using Project.Core.Repositories;
+using Project.Core.Services;
+using Project.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Services
+{
+    public class ProductService : BaseService<Product>, IProductService
+    {
+        public ProductService(IUnitOfWork unitOfWork, IRepository<Product> repository) :base(unitOfWork,repository)
+        {
+
+        }
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
+        }
+
+        
+    }
+}
