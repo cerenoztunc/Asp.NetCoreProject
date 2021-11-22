d9da0d866da0c629408c8ee6bf107eec3012247a
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-22 15:42:10 +0300

    Creating required Repositories in the Project.Data layer

---
+++ Project.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Core.Models;
+using Project.Core.Repositories;
+using Project.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data.Repositories
+{
+    public class CategoryRepository:BaseRepository<Category>, ICategoryRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public CategoryRepository(DbContext context):base(context)
+        {
+
+        }
+
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            var category = await _appDbContext.Categories.Include(x => x.Products).SingleOrDefaultAsync(x => x.Id == categoryId);
+            return category;
+        }
+    }
+}
---
+++ Project.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Core.Models;
+using Project.Core.Repositories;
+using Project.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data.Repositories
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public ProductRepository(DbContext context):base(context)
+        {
+
+        }
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            var product = await _appDbContext.Products.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == productId);
+            return product;
+        }
+    }
+}
