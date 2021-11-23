3d4cf700fac77d9f5cf94ed736b53226d9c0a6a5
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-23 15:02:59 +0300

    Adding Person entity

---
+++ Project.API/Controllers/PersonsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.Core.Models;
+using Project.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _service;
+        public PersonsController(IService<Person> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _service.GetAllAsync();
+            return Ok(persons);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(Person person)
+        {
+            var newPerson = await _service.AddAsync(person);
+            return Ok(newPerson);
+        }
+    }
+}
--- Project.Data/Context/AppDbContext.cs
+++ Project.Data/Context/AppDbContext.cs
@@ -16,11 +16,12 @@ namespace Project.Data.Context
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<Person> Persons { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new PersonConfiguration());
 
             modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1, 2 }));
             modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1, 2 }));
